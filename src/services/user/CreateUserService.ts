64bb92caa9cb082c2bbcf424c5d5d55da14255f0
YanGraim <yangraim@gmail.com>
2025-06-25 17:43:50 -0300

    feat: controllers e services

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,14 @@
+interface UserRequest {
+  name: String;
+  email: String;
+  password: String;
+}
+1
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+    console.log(name)
+    return { name: name }
+  }
+}
+
+export { CreateUserService }
