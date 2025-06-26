3c16a2ef73081a998002c75bc811900e9f3406b1
YanGraim <yangraim@gmail.com>
2025-06-26 13:37:33 -0300

    feat: cria middleware

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,7 +1,28 @@
 import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface PayLoad {
+  sub: string;
+}
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-  console.log("Chamou esse middleware");
+  //Receber o token
+  const authToken = req.headers.authorization;
 
-  return next();
+  if (!authToken) {
+    return res.status(401).end();
+  }
+
+  const [, token] = authToken.split(" ")
+
+  try {
+    //Validar o token
+    const { sub } = verify(
+      token,
+      process.env.JWT_SECRET,
+    ) as PayLoad;
+    return next();
+  } catch (error) {
+    return res.status(401).end();
+  }
 }
