9b4302ead9564e6b62441046c1eeb82c3380d1fe
YanGraim <yangraim@gmail.com>
2025-06-30 15:06:32 -0300

    feat: altera status da order para finalizar pedido

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
 import { DetailOrderController } from "./controllers/order/DetailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 const router = Router();
 
@@ -50,6 +51,7 @@ router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle);
 //PUT
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
+router.put("/order/finish", isAuthenticated, new FinishOrderController().handle);
 //GET
 router.get("/orders", isAuthenticated, new ListOrdersController().handle);
 router.get("/order/detail", isAuthenticated, new DetailOrderController().handle);
