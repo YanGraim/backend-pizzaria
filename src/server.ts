b35bb386c8ddfb95390cb18d96a9f2d102c6963f
YanGraim <yangraim@gmail.com>
2025-12-16 10:01:56 -0300

    feat: tratando erro.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response } from "express";
 import { router } from "./routes";
 
 const app = express();
@@ -9,8 +9,19 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
+app.use((error: Error, _: Request, res: Response) => {
+  if (error instanceof Error) {
+    return res.status(400).json({
+      error: error.message,
+    });
+  }
+  return res.status(500).json({
+    error: "Internal Server Error",
+  });
+});
+
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
-})
+});
